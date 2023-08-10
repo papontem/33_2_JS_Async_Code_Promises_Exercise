dd9326cc87ad69a166e3c5844fdcf2781c1e2423
papontemjetix <papontem@gmail.com>
2023-08-10 16:00:36 -0400

    added a get trivia button, and a unorded list to append trivia to, refactored code to work on button click, and only works after the document is fully loaded.

--- js/nums/nums.js
+++ js/nums/nums.js
@@ -1,75 +1,65 @@
 console.log("HELLO NUMBERS!");
 
-// function myAsyncFunction() {
-//     // return a new Promise
-//     return new Promise((resolve, reject) => {
-//         /*
-    
-//         DO ASYNC STUFF HERE
-    
-//         */
-    
-//         // if it succeeds, call the resolve callback
-//         resolve(/* success value*/);
-    
-//         // if it fails, call the reject callback
-//         reject(/* fail value*/);
-//     });
-//     }
-
-
-
 // PART 1.1
 // pick your favorite number or any number really
-const num = 0;
-console.log(`Current number: ${num}`);
+// const num = 0;
+// console.log(`Current number: ${num}`);
 
-const baseUrl = 'http://numbersapi.com/' ;
+// const baseUrl = 'http://numbersapi.com/' ;
 
-// Create the URL to get json resposnse from the API
-// by adding json query at the end with our selected number
-const randFactJsonURL = baseUrl + `${num}?json`;
-const randFact = axios.get(randFactJsonURL)
+// // Create the URL to get json resposnse from the API
+// // by adding json query at the end with our selected number
+// const randFactJsonURL = baseUrl + `${num}?json`;
+// const randFact = axios.get(randFactJsonURL)
 
-// PROMISE CHAINING
-randFact
-    .then( res => {
-        // console.log("RESOLVED!!\nresponde.data:",res.data)
-        return res.data
-    })
-    .then(data => {
-        // console.log(`data.text: ${data.text}`);
-        console.log(`Random Fact about # ${data.number}: ${data.text}`);
+// // PROMISE CHAINING
+// randFact
+//     .then( res => {
+//         // console.log("RESOLVED!!\nresponde.data:",res.data)
+//         return res.data
+//     })
+//     .then(data => {
+//         // console.log(`data.text: ${data.text}`);
+//         console.log(`Random Fact about # ${data.number}: ${data.text}`);
 
-    })
-    .catch(error => console.error("REJECTED!!\nERROR:",error));
+//     })
+//     .catch(error => console.error("REJECTED!!\nERROR:",error));
 
 
 // PART 1.2
-// make a list of numbers or use the random list of numbers
+$(document).ready(function () {
+    const baseUrl = 'http://numbersapi.com/';
+    // function callback for if you wish get a random number 0-100
+    const getRandomInt = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
+    // make a list of numbers or use the random list of numbers
+    let numListPromises = [];
+    
+    // button listener to get more fatcs
+    $('#getRandomFactsBtn').on('click', function () {
 
-let numListPromises = [];
-// function for if you wish to make a list of random numbers 0-100
-function getRandomInt(min=0, max=100) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+        numListPromises = []
 
-for (let i = 0; i < 3; i++) {
-    numListPromises.push(
-        // pick between a) sequence or b) a random list 
-        // axios.get(baseUrl + `${i}?json`) // a) sequence
-        axios.get(baseUrl + `${getRandomInt()}?json`)  // b) random list
-    );
-}
+        // for loop to push 3 nums api get promises into the nums list
+        for (let i = 0; i < 3; i++) {
+            numListPromises.push(
+                // pick between a) sequence or b) a random list 
+                // axios.get(baseUrl + `${i}?json`) // a) sequence
+                axios.get(baseUrl + `${getRandomInt()}?json`)  // b) random list
+            );
+        }
+        
+        Promise.all(numListPromises)
+        .then(numApiResponses => {
+            // uncomment if you want the list to refresh
+            // $('#nums-fact-list').empty();
 
-// Go through all the promises and request them 
-Promise.all(numListPromises)
-.then(numApiResponses => {
-    console.log("RESOLVED!!",numApiResponses)
-    for(res of numApiResponses){
-        console.log(`Random fact about # ${res.data.number} : ${res.data.text}`)
-    }
-})
-// first promise that gets rejected, print out its error
-.catch(error => console.error("REJECTED!!\nERROR:",error));
-
+            numApiResponses.forEach(res => {
+                const randFact = `Random fact about # ${res.data.number}: ${res.data.text}`;
+                $('#nums-fact-list').append(`<li>${randFact}</li>`);
+            });
+        })
+        .catch(error => {
+            console.error('REJECTED!!\nERROR:', error);
+        });
+    });
+});
